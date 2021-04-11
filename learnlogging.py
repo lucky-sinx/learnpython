cec13fa33d640253dd4d630d09ddd5869be2d852
lucky-sinx <2549261744@qq.com>
2021-04-11 22:08:22 +0800

    学习logging

--- learnlogging.py
+++ learnlogging.py
@@ -1,1 +1,10 @@
 import logging
+# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG) # 对logging级别进行设置
+logger=logging.getLogger(__name__)
+
+logger.info('Start reading database')
+res={'join':55,'tom':66}
+logger.debug('Records:%s'%res)
+logger.info('update:')
+logger.info('finish update')
